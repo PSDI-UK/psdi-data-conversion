c545256e431f9f2bc8a03d5cebfd03b8223207e7
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-12-06 09:59:49 +0000

    Bugfix for certain options disappearing
    Certain font and other options would disappear if
    they were selected and the Accessibility page
    were returned to, due to option text not matching
    value in some cases (due to whitespace). This
    fixes that by trimming whitespace from the text

--- static/javascript/accessibility.js
+++ static/javascript/accessibility.js
@@ -188,15 +188,15 @@ function applySettings(event) {
     sessionStorage.setItem("lightBack", s.getPropertyValue('--ifm-background-color'));
     sessionStorage.setItem("darkBack", s.getPropertyValue('--ifm-color-primary'));
 
-    sessionStorage.setItem("fontOpt", $("#font").find(":selected").text());
-    sessionStorage.setItem("sizeOpt", $("#size").find(":selected").text());
-    sessionStorage.setItem("weightOpt", $("#weight").find(":selected").text());
-    sessionStorage.setItem("letterOpt", $("#letter").find(":selected").text());
-    sessionStorage.setItem("lineOpt", $("#line").find(":selected").text());
-    sessionStorage.setItem("darkColourOpt", $("#dark-colour").find(":selected").text());
-    sessionStorage.setItem("lightColourOpt", $("#light-colour").find(":selected").text());
-    sessionStorage.setItem("lightBackOpt", $("#light-background").find(":selected").text());
-    sessionStorage.setItem("darkBackOpt", $("#dark-background").find(":selected").text());
+    sessionStorage.setItem("fontOpt", $("#font").find(":selected").text().trim());
+    sessionStorage.setItem("sizeOpt", $("#size").find(":selected").text().trim());
+    sessionStorage.setItem("weightOpt", $("#weight").find(":selected").text().trim());
+    sessionStorage.setItem("letterOpt", $("#letter").find(":selected").text().trim());
+    sessionStorage.setItem("lineOpt", $("#line").find(":selected").text().trim());
+    sessionStorage.setItem("darkColourOpt", $("#dark-colour").find(":selected").text().trim());
+    sessionStorage.setItem("lightColourOpt", $("#light-colour").find(":selected").text().trim());
+    sessionStorage.setItem("lightBackOpt", $("#light-background").find(":selected").text().trim());
+    sessionStorage.setItem("darkBackOpt", $("#dark-background").find(":selected").text().trim());
 
     alert("The settings have been applied to the entire website.");
 }
