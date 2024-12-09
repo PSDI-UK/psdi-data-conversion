1c920abb32e9f1b2ff03fd30338cd292789e5152
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-12-09 10:04:07 +0000

    Expand test to be more detailed

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -8,21 +8,37 @@ Tests of functions relating to logging
 import re
 import time
 
-from app import get_date_time
+from app import get_date, get_date_time, get_time
 
 
 def test_date_time():
     """Tests of getting the current date and time
     """
 
-    # Use a regex match to test that the date is in the right format
-    ex_re = re.compile(r"\d{4}-[0-1]\d-[0-3]\d")
+    # Use a regex match to test that the date and time are in the right format
+    date_re_raw = r"\d{4}-[0-1]\d-[0-3]\d"
+    time_re_raw = r"[0-2]\d:[0-5]\d:[0-5]\d"
+    datetime_re_raw = f"{date_re_raw} {time_re_raw}"
 
-    dt_str_1 = get_date_time()
-    assert ex_re.match(dt_str_1)
+    date_re = re.compile(date_re_raw)
+    time_re = re.compile(time_re_raw)
+    datetime_re = re.compile(datetime_re_raw)
+
+    date_str_1 = get_date()
+    time_str_1 = get_time()
+    datetime_str_1 = get_date_time()
+
+    assert date_re.match(date_str_1)
+    assert time_re.match(time_str_1)
+    assert datetime_re.match(datetime_str_1)
 
     # Test that the time changes after a second, and is still in the correct format
     time.sleep(1)
-    dt_str_2 = get_date_time()
-    assert ex_re.match(dt_str_2)
-    assert dt_str_2 != dt_str_1
+    time_str_2 = get_time()
+    datetime_str_2 = get_date_time()
+
+    assert time_re.match(time_str_2)
+    assert datetime_re.match(datetime_str_2)
+
+    assert time_str_2 != time_str_1
+    assert datetime_str_2 != datetime_str_1
