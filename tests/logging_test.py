7fd5df3c4e7b35da06d6283a10df5a6ea6bd4402
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-12-09 09:53:27 +0000

    Add unit tests of get_date_time

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -5,8 +5,24 @@ Created 2024-12-09 by Bryan Gillis.
 Tests of functions relating to logging
 """
 
+import re
+import time
 
-def test_dummy():
-    """Dummy test to confirm pytest is working
+from app import get_date_time
+
+
+def test_date_time():
+    """Tests of getting the current date and time
     """
-    return
+
+    # Use a regex match to test that the date is in the right format
+    ex_re = re.compile(r"\d{4}-[0-1]\d-[0-3]\d")
+
+    dt_str_1 = get_date_time()
+    assert ex_re.match(dt_str_1)
+
+    # Test that the time changes after a second, and is still in the correct format
+    time.sleep(1)
+    dt_str_2 = get_date_time()
+    assert ex_re.match(dt_str_2)
+    assert dt_str_2 != dt_str_1
