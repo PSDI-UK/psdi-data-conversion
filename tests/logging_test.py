afcda3f6d4aed962bdbd7662006e9468fdd8806c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-12-09 09:39:31 +0000

    Add stub for unit tests

---
+++ tests/logging_test.py
@@ -0,0 +1,12 @@
+"""@file psdi-data-conversion/tests/logging_test.py
+
+Created 2024-12-09 by Bryan Gillis.
+
+Tests of functions relating to logging
+"""
+
+
+def test_dummy():
+    """Dummy test to confirm pytest is working
+    """
+    return
