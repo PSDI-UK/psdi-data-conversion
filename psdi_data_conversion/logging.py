188fc1fdf6b7eb5e304754e28fe1882543dc27af
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2024-12-09 13:53:52 +0000

    Add class for specialized logger
    We want to log to both a global and local log
    file, so we create a specialized logger class
    to handle this

---
+++ psdi_data_conversion/logging.py
@@ -0,0 +1,67 @@
+"""@file psdi-data-conversion/psdi_data_conversion/logging.py
+
+Created 2024-12-09 by Bryan Gillis.
+
+Functions and classes related to logging
+"""
+
+import logging
+
+# Global file to log any errors that occur
+ERROR_LOG_FILENAME = "error_log.txt"
+GLOBAL_ERROR_LOG = f"./{ERROR_LOG_FILENAME}"
+
+logging.basicConfig(filename=ERROR_LOG_FILENAME)
+
+
+class DataConversionLogger(logging.Logger):
+    """Logger which logs to both a global file and a local file
+    """
+
+    def __init__(self, name, local_log_filename, level=logging.NOTSET):
+        """Creates a logger as a child class of `logging.Logger`
+
+        Parameters
+        ----------
+        name : str
+            The desired logging channel for this logger. Should be a period-separated string such as "input.files" etc.
+        local_log_filename : str
+            The filename of a file local to this process to use for logging.
+        level : int, optional
+            The desired logging level, using one of the constants defined in the `logging` module
+        """
+
+        # Set up the global logger
+        super().__init__(name, level)
+        global_handler = logging.FileHandler(ERROR_LOG_FILENAME)
+        self.addHandler(global_handler)
+
+        # Set up the local logger
+        self.local_logger = logging.getLogger(f"{name}-local")
+        local_handler = logging.FileHandler(local_log_filename)
+        self.local_logger.addHandler(local_handler)
+        self.local_logger.setLevel(level)
+
+    def setLevel(self, level):
+        """Override the `setLevel` method to set the same level with both the global and local loggers
+        """
+        super().setLevel(level)
+        self.local_logger.setLevel(level)
+
+    def log(self, level, msg, *args, **kwargs):
+        """Override the `log` method to log with both the global and local loggers
+        """
+        super().log(level, msg, *args, **kwargs)
+        self.local_logger.log(level, msg, *args, **kwargs)
+
+
+def getLogger(name):
+    """Gets the `DataConversionLogger` with the provided name if one exists, and creates one if not
+    """
+
+    old_logger_class = logging.getLoggerClass()
+    try:
+        logging.setLoggerClass(DataConversionLogger)
+        return logging.getLogger(name)
+    finally:
+        logging.setLoggerClass(old_logger_class)
