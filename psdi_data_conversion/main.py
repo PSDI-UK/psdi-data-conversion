2da79e57081099376a1cbdb9a3c8f01a17fd5975
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-01-14 11:16:48 +0000

    Add dummy python script for conversion

---
+++ psdi_data_conversion/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+"""@file psdi_data_conversion/main.py
+
+Created 2025-01-14 by Bryan Gillis.
+
+Entry-point file for the command-line interface for data conversion.
+"""
+
+import logging
+from argparse import ArgumentParser
+
+logger = logging.getLogger(__name__)
+
+
+def get_argument_parser():
+    """Get an argument parser for this script.
+
+    Returns
+    -------
+    parser : ArgumentParser
+        An argument parser set up with the allowed command-line arguments for this script.
+    """
+
+    parser = ArgumentParser()
+
+    parser.add_argument("--log-level", type=str, default="INFO",
+                        help="The desired level to log at. Allowed values are: 'DEBUG', 'INFO', 'WARNING', 'ERROR, "
+                             "'CRITICAL'. Default: 'INFO'")
+
+    return parser
+
+
+def parse_args():
+    """Parses arguments for this executable.
+
+    Returns
+    -------
+    args : Namespace
+        The parsed arguments.
+    """
+
+    parser = get_argument_parser()
+
+    args = parser.parse_args()
+
+    return args
+
+
+def run_from_args(args):
+    """Workhorse function to perform primary execution of this script, using the provided parsed arguments.
+
+    Parameters
+    ----------
+    args : Namespace
+        The parsed arguments for this script.
+    """
+
+    logger.info("This is currently a dummy executable, with functionality TBD.")
+
+
+def main():
+    """Standard entry-point function for this script.
+    """
+
+    args = parse_args()
+
+    logging.basicConfig(level=args.log_level)
+
+    logger.info("#")
+    logger.info("# Beginning execution of script `%s`", __file__)
+    logger.info("#")
+
+    run_from_args(args)
+
+    logger.info("#")
+    logger.info("# Finished execution of script `%s`", __file__)
+    logger.info("#")
+
+
+if __name__ == "__main__":
+
+    main()
