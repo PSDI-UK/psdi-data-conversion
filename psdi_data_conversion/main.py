76dbea51d20e551408afacdb1516527607e32cd8
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-01-14 12:10:25 +0000

    Add arguments for CLI script

--- psdi_data_conversion/main.py
+++ psdi_data_conversion/main.py
@@ -24,6 +24,40 @@ def get_argument_parser():
 
     parser = ArgumentParser()
 
+    # Positional arguments
+    parser.add_argument("l_args", type=str, nargs="*",
+                        help="Normally, file(s) to be converted. Filenames should be provided as either relative to "
+                             "the input directory (default current directory) or absolute. If the '-l' or '--list' "
+                             "flag is set, instead the name of a converter can be used here to get information on it.")
+
+    # Keyword arguments for standard conversion
+    parser.add_argument("-f", "--from", type=str, default=None,
+                        help="The input (convert from) file extension (e.g., smi). If not provided, will attempt to "
+                             "auto-detect format.")
+    parser.add_argument("-i", "--in", type=str, default=None,
+                        help="The directory containing the input file(s), default current directory.")
+    parser.add_argument("-t", "--to", type=str,
+                        help="The output (convert to) file extension (e.g., cmi).")
+    parser.add_argument("-a", "--at", type=str, default=None,
+                        help="The directory where output files should be created, default same as input directory.")
+    parser.add_argument("-w", "--with", type=str, default="Open Babel",
+                        help="The converter to be used (default 'Open Babel').")
+    parser.add_argument("--flags", type=str, default="",
+                        help="Any command-line flags to be provided to the converter. For information on the flags "
+                             "accepted by a converter, call this script with '-l <converter name>'.")
+
+    # Keyword arguments for alternative functionality
+    parser.add_argument("-l", "--list", type=bool, action="store_true",
+                        help="If provided alone, lists all available converters. If the name of a converter is "
+                             "provided, gives information on the converter and any command-line flags it accepts.")
+
+    # Logging/stdout arguments
+    parser.add_argument("-q", "--quiet", type=bool, action="store_true",
+                        help="If set, all output aside from errors will be suppressed and no log file will be "
+                             "generated.")
+    parser.add_argument("-l", "--log-file", type=str, default=None,
+                        help="The name of the file to log to. If not provided, the log file will be named after the "
+                             "first input file (+'.log') and placed in the current directory.")
     parser.add_argument("--log-level", type=str, default="WARNING",
                         help="The desired level to log at. Allowed values are: 'DEBUG', 'INFO', 'WARNING', 'ERROR, "
                              "'CRITICAL'. Default: 'INFO'")
