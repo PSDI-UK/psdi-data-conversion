ef3f8879c9cf6f8c7fb63dd886b4e8aa4fa27388
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-01-15 15:02:12 +0000

    Add helper function to get parsed args from an input string

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -5,6 +5,20 @@ Created 2025-01-15 by Bryan Gillis.
 Tests of the command-line interface
 """
 
+import shlex
+import sys
+from unittest.mock import patch
+
+from psdi_data_conversion.main import parse_args
+
+
+def get_parsed_args(s):
+    """Performs argument parsing on a string which represents what the arguments would be after the function call
+    """
+    s = "-c " + s
+    with patch.object(sys, 'argv', shlex.split(s)):
+        return parse_args
+
 
 def test_input_validity():
     """Unit tests to ensure that the CLI properly checks for valid input
