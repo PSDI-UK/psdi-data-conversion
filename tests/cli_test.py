730b4b0095e12ee19b0d5683ff7f0a5834d76d9c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-01-15 10:48:15 +0000

    Add skeleton for unit tests of CLI

---
+++ tests/cli_test.py
@@ -0,0 +1,21 @@
+"""@file psdi-data-conversion/tests/cli_test.py
+
+Created 2025-01-15 by Bryan Gillis.
+
+Tests of the command-line interface
+"""
+
+
+def test_input_validity():
+    """Unit tests to ensure that the CLI properly checks for valid input
+    """
+    # TODO
+    pass
+
+
+def test_input_processing():
+    """Unit tests to ensure that the CLI properly processes input arguments to determine values that are needed but
+    weren't provided
+    """
+    # TODO
+    pass
