4c53ecebb0f06759de3755593acdfd4451db9b1b
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-01-20 15:13:53 +0000

    Clean up passing of MAX_FILESIZE

--- run_local.sh
+++ run_local.sh
@@ -2,7 +2,7 @@
 
 # The envvar MAX_FILESIZE can be used to set the maximum allowed filesize in MB - 0 indicates no maximum
 if [ -z $MAX_FILESIZE ]; then
-  MAX_FILESIZE="0"
+  export MAX_FILESIZE=0
 fi
 
 # Uncomment the following line to enable debug mode
@@ -12,4 +12,4 @@ fi
 
 PACKAGE_PATH=`python -c "import psdi_data_conversion; print(psdi_data_conversion.__path__[0])"`
 cd $PACKAGE_PATH/..
-MAX_FILESIZE=$MAX_FILESIZE python -m flask --app psdi_data_conversion/app.py run
+python -m flask --app psdi_data_conversion/app.py run
