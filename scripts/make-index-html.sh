91f31a13f72f6663d6860b6fff8b2ccd31d3f644
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-01-22 09:37:18 +0000

    Use copy_html script with new options

--- scripts/make-index-html.sh
+++ scripts/make-index-html.sh
@@ -11,16 +11,27 @@ else
     echo "No configuration file found at $CONF_FILE; configuration will be controlled by environmental variables"
 fi
 
-# Files to make copies of will be found in the HTML directory
-ORIG_HEADER_FILENAME=$TARGET_HTML_DIR/psdi-common-header.html
-ALT_HEADER_FILENAME=$TARGET_HTML_DIR/psdi-common-header-index.html
-cp $ORIG_HEADER_FILENAME $ALT_HEADER_FILENAME
+if [ -z $TARGET_BASE_DIR ]; then
+  TARGET_BASE_DIR=$DEFAULT_TARGET_BASE_DIR
+fi
+if [ -z $TARGET_HTML_DIR ]; then
+  TARGET_HTML_DIR=$TARGET_BASE_DIR/html
+fi
 
-ORIG_FOOTER_FILENAME=$TARGET_HTML_DIR/psdi-common-footer.html
-ALT_FOOTER_FILENAME=$TARGET_HTML_DIR/psdi-common-footer-index.html
-cp $ORIG_FOOTER_FILENAME $ALT_FOOTER_FILENAME
+INDEX_HTML_DIR=$TARGET_HTML_DIR/index-versions
 
-# Update paths in both header and footer
-for FILENAME in $ALT_HEADER_FILENAME $ALT_FOOTER_FILENAME; do
-    
-done
+# The address that clicking on the PSDI brand on the left side of the header should link to. Default: "./", which will
+# most likely be the home page of the project
+export BRAND_LINK="."
+
+# The location of a file containing HTML links to be added to the header, which will appear on the right side of it.
+# Default: (no links will be added)
+export HEADER_LINKS_SOURCE="$INDEX_HTML_DIR/header-links.html"
+
+# The address of a directory containing the images referenced by the header and footer. By default, this will link
+# to the live versions at https://psdi-uk.github.io/psdi-common-style/img/. This can be set to e.g. "./img" to instead
+# link to local copies of these images
+export IMG_LOC="$TARGET_BASE_DIR/img"
+
+# Make new copies 
+TARGET_DIR=$INDEX_HTML_DIR $SCRIPTS/copy_html.sh
