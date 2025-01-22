b026c08ca6af39b48307de9563ea6c92dfa98d4d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-01-22 09:47:30 +0000

    Get index script working

--- scripts/make-index-html.sh
+++ scripts/make-index-html.sh
@@ -18,20 +18,16 @@ if [ -z $TARGET_HTML_DIR ]; then
   TARGET_HTML_DIR=$TARGET_BASE_DIR/html
 fi
 
+# Set alternate variables for the data that will differ for the index version of files
 INDEX_HTML_DIR=$TARGET_HTML_DIR/index-versions
-
-# The address that clicking on the PSDI brand on the left side of the header should link to. Default: "./", which will
-# most likely be the home page of the project
 export BRAND_LINK="."
-
-# The location of a file containing HTML links to be added to the header, which will appear on the right side of it.
-# Default: (no links will be added)
 export HEADER_LINKS_SOURCE="$INDEX_HTML_DIR/header-links.html"
-
-# The address of a directory containing the images referenced by the header and footer. By default, this will link
-# to the live versions at https://psdi-uk.github.io/psdi-common-style/img/. This can be set to e.g. "./img" to instead
-# link to local copies of these images
 export IMG_LOC="$TARGET_BASE_DIR/img"
+
+# Find the location of the copy_html.sh script
+PACKAGE_FILENAME=$ROOTDIR/psdi-assets.tar.gz
+ASSET_SUBDIR=`tar tf $PACKAGE_FILENAME | head -n 1`
+SCRIPTS=$ROOTDIR/$ASSET_SUBDIR/scripts
 
 # Make new copies 
 TARGET_DIR=$INDEX_HTML_DIR $SCRIPTS/copy_html.sh
