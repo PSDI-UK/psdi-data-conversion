cf8202269928a6e266d65fe1eca193ec815e74ee
RayWhorley <r.p.whorley@soton.ac.uk>
2025-01-25 09:46:13 +0000

    The rest of the c2x conversions have been added to the database, and blur() has been applied when selecting conversion option flags.

--- psdi_data_conversion/static/javascript/convert.js
+++ psdi_data_conversion/static/javascript/convert.js
@@ -1,6 +1,6 @@
 /*
   convert.js
-  Version 1.0, 12th December 2024
+  Version 1.0, 24th January 2025
 
   This is the JavaScript which makes the convert.htm gui work.
 */
@@ -21,12 +21,19 @@ $(document).ready(function () {
 
     $('input[name="coordinates"]').change(coordOptionAvailability);
     $("#uploadButton").click(submitFile);
+    $("#inFlags").click(blurBox);
+    $("#outFlags").click(blurBox);
 
     getFlags("in", in_str);
     getFlags("out", out_str);
     getFlags("in_arg", in_str);
     getFlags("out_arg", out_str);
 });
+
+// On clicking on an option flag, the select box loses focus so that the light font and dark background (Chrome) appears immediately.
+function blurBox(event) {
+    this.blur();
+}
 
 // On ticking a checkbox, a text box for entry of an option flag argument appears next to it. On unticking, the text box disappears.
 function enterArgument(event) {
