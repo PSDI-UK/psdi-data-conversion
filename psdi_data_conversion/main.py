cec571b80a19125973091f6acebc0de8d528167c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-01-27 10:16:26 +0000

    Clarify log file can be relative or fully qualified

--- psdi_data_conversion/main.py
+++ psdi_data_conversion/main.py
@@ -225,8 +225,10 @@ def get_argument_parser():
 
     # Logging/stdout arguments
     parser.add_argument("-l", "--log-file", type=str, default=None,
-                        help="The name of the file to log to. If not provided, the log file will be named after the "
-                             "first input file (+'.log') and placed in the current directory.")
+                        help="The name of the file to log to. This can be provided relative to the current directory "
+                        "(e.g. '-l ../logs/log-file.txt') or fully qualified (e.g. /path/to/log-file.txt). "
+                        "If not provided, the log file will be named after the =first input file (+'.log') and placed "
+                        "in the current directory.")
     parser.add_argument("--log-mode", type=str, default=const.LOG_SIMPLE,
                         help="How logs should be stores. Allowed values are: \n"
                         "- 'full' - Multi-file logging, only recommended when running as a public web app"
