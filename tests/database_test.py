9e4103bce4ff404408d2ca988db9ea87bdd509b9
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-02-03 14:29:03 +0000

    Add test of converter info

--- tests/database_test.py
+++ tests/database_test.py
@@ -6,7 +6,10 @@ Unit tests relating to using the database
 """
 
 
-from psdi_data_conversion.database import get_database
+import pytest
+from psdi_data_conversion.converter import L_REGISTERED_CONVERTERS
+from psdi_data_conversion.converters.base import FileConverter
+from psdi_data_conversion.database import ConverterInfo, FileConverterDatabaseException, get_database
 
 
 def test_load():
@@ -18,3 +21,37 @@ def test_load():
 
     # We should only get one database created, and any additional calls to `get_database()` should return the same
     assert db2 is db1
+
+
+def test_open_babel_info():
+    """Test that we can get the expected information on each converter
+    """
+
+    database = get_database()
+
+    for name in L_REGISTERED_CONVERTERS:
+
+        converter_info = database.converter_info[name]
+
+        # Check database is properly set as parent
+        assert converter_info.parent == database
+
+        # Check name matches
+        assert converter_info.name == name
+
+        # Check ID is of proper type and an allowed value
+        assert isinstance(converter_info.id, int)
+        assert converter_info.id > 0
+
+        # Check description has some text in it
+        assert isinstance(converter_info.description, str)
+        assert len(converter_info.description) > 0
+
+        # Check URL appears reasonable
+        assert isinstance(converter_info.url, str)
+        assert "http" in converter_info.url
+
+    # Check we get expected error if a converter isn't in the database
+    with pytest.raises(FileConverterDatabaseException):
+        converter_info = ConverterInfo(FileConverter, database, database._d_data)
+        _ = converter_info.id
