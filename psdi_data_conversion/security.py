79337c68f465e9743e3f5339db7882e3fd842b72
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-02-24 14:41:02 +0000

    Add module for security functions

---
+++ psdi_data_conversion/security.py
@@ -0,0 +1,29 @@
+"""@file psdi_data_conversion/security.py
+
+Created 2025-02-24 by Bryan Gillis.
+
+Functions related to security
+"""
+
+import re
+
+# Short list of safe allowed characters:
+# \w: All letters and digits
+# \s: All whitespace characters
+# .: The period character
+# \-: The hyphen character
+_SAFE_CHARS = r"[\w\s.\-]"
+SAFE_CHAR_RE = re.compile(_SAFE_CHARS)
+SAFE_STRING_RE = re.compile(f"{_SAFE_CHARS}*")
+
+
+def char_is_safe(s: str):
+    """Checks whether a character is in the set of predefined safe characters.
+    """
+    return bool(SAFE_CHAR_RE.match(s))
+
+
+def string_is_safe(s: str):
+    """Checks whether a string contains only characters in the predefined list of safe characters.
+    """
+    return bool(SAFE_STRING_RE.match(s))
