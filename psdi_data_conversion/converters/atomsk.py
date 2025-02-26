72c7f208285e23f1e19a14b895bd36885e467cec
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-02-26 16:06:21 +0000

    Add more detailed info

--- psdi_data_conversion/converters/atomsk.py
+++ psdi_data_conversion/converters/atomsk.py
@@ -17,9 +17,16 @@ class AtoFileConverter(ScriptFileConverter):
     name = CONVERTER_ATO
     script = "atomsk.sh"
     required_bin = "atomsk"
-    info = "See documentation for Atomsk at https://atomsk.univ-lille.fr/"
+    info = ("An Atomsk binary compiled for 64-bit Linux systems is distributed with this package. It may be registered "
+            "on other systems by compiling it locally and adding the compiled 'atomsk' binary (with this exact name - "
+            "rename it or make a symbolic link to it if necessary) to your $PATH.\n"
+            "\n"
+            "Atomsk is licensed under GPLv3, the full text of which may be found at "
+            "https://www.gnu.org/licenses/gpl-3.0.en.html. Its binary is redistributed here under the terms of this "
+            "license, and any further redistribution must also follow these terms. Its corresponding source code "
+            "may be found at https://github.com/pierrehirel/atomsk/")
 
 
 # Assign this converter to the `converter` variable - this lets the psdi_data_conversion.converter module detect and
-# register it, making it available for use by the CLI and web app
+# register it, making it available for use by the command-line script, python library, and web app
 converter = AtoFileConverter
