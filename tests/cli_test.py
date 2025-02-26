6daf7c6b4abca2d259dbc423ff74ad05c2b5637d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-02-26 14:53:20 +0000

    Fix test to expect warning on Mac

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -20,6 +20,7 @@ from psdi_data_conversion.converters.openbabel import (CONVERTER_OB, COORD_GEN_K
                                                        DEFAULT_COORD_GEN, DEFAULT_COORD_GEN_QUAL)
 from psdi_data_conversion.database import (get_conversion_quality, get_converter_info, get_in_format_args,
                                            get_out_format_args, get_possible_converters, get_possible_formats)
+from psdi_data_conversion.dist import MAC_LABEL, get_dist
 from psdi_data_conversion.file_io import unpack_zip_or_tar
 from psdi_data_conversion.log_utility import string_with_placeholders_matches
 from psdi_data_conversion.main import FileConverterInputException, main, parse_args
@@ -223,11 +224,17 @@ def test_detail_converter(capsys):
     assert "Traceback" not in captured.err
 
     # Test that we can also provide the converter name with -w/--with
-    run_with_arg_string(f"-l -w {CONVERTER_ATO}")
+    run_with_arg_string(f"-l -w {CONVERTER_C2X}")
     captured = capsys.readouterr()
     assert not captured.err
     assert CONVERTER_ATO in captured.out
     assert const.CONVERTER_DEFAULT not in captured.out
+
+    # On Mac, test that we get a warning with Atomsk
+    if get_dist() == MAC_LABEL:
+        run_with_arg_string(f"-l -w {CONVERTER_ATO}")
+        captured = capsys.readouterr()
+        assert "WARNING" in captured.err
 
 
 def test_get_converters(capsys):
