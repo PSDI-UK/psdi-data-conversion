588ae2b3d4730247c79a4a579523080976af7632
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-02-27 15:29:25 +0000

    Add test and fix bug with c2x

--- tests/converter_test.py
+++ tests/converter_test.py
@@ -315,8 +315,10 @@ class TestConverter:
         self.check_file_status(input_exist=False, output_exist=True)
 
     def test_atomsk(self):
-        """Run a test of the Atomsk converter on a straightforward `.pdb` to `.cif` conversion
+        """Run tests of Atomsk on some conversions we expect to succeed without issue
         """
+
+        # pdb to cif
 
         self.get_input_info(filename="hemoglobin.pdb",
                             to_format="cif")
@@ -326,8 +328,15 @@ class TestConverter:
         # Check that the input file has not been deleted and the output file exists where we expect it to
         self.check_file_status(input_exist=True, output_exist=True)
 
+        # cif to xyz
+
+        self.get_input_info(filename="nacl.cif",
+                            to_format="xyz")
+        self.run_converter(name=CONVERTER_ATO)
+        self.check_file_status(input_exist=True, output_exist=True)
+
     def test_c2x(self):
-        """Run a test of the C2X converter on a straightforward `.pdb` to `.cif` conversion
+        """Run tests of c2x on some conversions we expect to succeed without issue
         """
 
         self.get_input_info(filename="hemoglobin.pdb",
@@ -336,6 +345,13 @@ class TestConverter:
         self.run_converter(name=CONVERTER_C2X)
 
         # Check that the input file has not been deleted and the output file exists where we expect it to
+        self.check_file_status(input_exist=True, output_exist=True)
+
+        # cif to cml
+
+        self.get_input_info(filename="nacl.cif",
+                            to_format="cml")
+        self.run_converter(name=CONVERTER_C2X)
         self.check_file_status(input_exist=True, output_exist=True)
 
     def test_xyz_to_inchi_err(self):
