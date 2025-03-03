8c3f32e80deafde4119712e4b02fce7b2e6c8fd3
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-03-03 14:44:39 +0000

    Run with pytest

--- tests/selenium/selenium_tests.py
+++ tests/selenium/selenium_tests.py
@@ -4,9 +4,9 @@
 
 import os
 import time
-import unittest
 
 from pathlib import Path
+import pytest
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
@@ -31,109 +31,106 @@ if (origin is None):
 # Loop timeout at 10 seconds
 TIMEOUT = 10
 
-# print(f"origin: {origin}")
-# print(f"driver: {driver_path}")
 
-driver = webdriver.Firefox(service=FirefoxService(driver_path))
+@pytest.fixture(scope="module")
+def driver():
+    ff_driver = webdriver.Firefox(service=FirefoxService(driver_path))
+    yield ff_driver
+    ff_driver.quit()
 
 
-class TestBasicOperations(unittest.TestCase):
+def test_initial_frontpage(driver):
 
-    def test_initial_frontpage(self):
+    driver.get(f"{origin}/")
 
-        driver.get(f"{origin}/")
+    # Check that the front page contains the header "Data Conversion Service".
 
-        # Check that the front page contains the header "Data Conversion Service".
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//header")))
+    element = driver.find_element(By.XPATH, "//header//h5")
+    assert element.text == "Data Conversion Service"
 
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//header")))
-        element = driver.find_element(By.XPATH, "//header//h5")
-        self.assertEqual(element.text, "Data Conversion Service")
+    # Check that the 'from' and 'to' lists contains "abinit" and "acesin" respectively.
 
-        # Check that the 'from' and 'to' lists contains "abinit" and "acesin" respectively.
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        (By.XPATH, "//select[@id='fromList']/option")))
+    driver.find_element(By.XPATH, "//select[@id='fromList']/option[contains(.,'abinit: ABINIT output')]")
 
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-            (By.XPATH, "//select[@id='fromList']/option")))
-        driver.find_element(By.XPATH, "//select[@id='fromList']/option[contains(.,'abinit: ABINIT output')]")
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//select[@id='toList']/option")))
+    driver.find_element(By.XPATH, "//select[@id='toList']/option[contains(.,'acesin: ACES input')]")
 
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//select[@id='toList']/option")))
-        driver.find_element(By.XPATH, "//select[@id='toList']/option[contains(.,'acesin: ACES input')]")
+    # Check that the available conversions list is empty.
 
-        # Check that the available conversions list is empty.
+    with pytest.raises(NoSuchElementException):
+        driver.find_element(By.XPATH, "//select[@id='success']/option")
 
-        with self.assertRaises(NoSuchElementException):
-            driver.find_element(By.XPATH, "//select[@id='success']/option")
 
-    def test_cdxml_to_inchi_conversion(self):
+def test_cdxml_to_inchi_conversion(driver):
 
-        test_file = "standard_test"
+    test_file = "standard_test"
 
-        input_file = Path.cwd().joinpath("files", f"{test_file}.cdxml")
-        output_file = Path.home().joinpath("Downloads", f"{test_file}.inchi")
-        log_file = Path.home().joinpath("Downloads", f"{test_file}.log.txt")
+    input_file = Path.cwd().joinpath("files", f"{test_file}.cdxml")
+    output_file = Path.home().joinpath("Downloads", f"{test_file}.inchi")
+    log_file = Path.home().joinpath("Downloads", f"{test_file}.log.txt")
 
-        # Remove test files from Downloads directory if they exist.
+    # Remove test files from Downloads directory if they exist.
 
-        if (Path.is_file(log_file)):
-            Path.unlink(log_file)
+    if (Path.is_file(log_file)):
+        Path.unlink(log_file)
 
-        if (Path.is_file(output_file)):
-            Path.unlink(output_file)
+    if (Path.is_file(output_file)):
+        Path.unlink(output_file)
 
-        driver.get(f"{origin}/")
+    driver.get(f"{origin}/")
 
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-            (By.XPATH, "//select[@id='fromList']/option")))
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        (By.XPATH, "//select[@id='fromList']/option")))
 
-        # Select cdxml from the 'from' list.
+    # Select cdxml from the 'from' list.
 
-        driver.find_element(By.XPATH, "//select[@id='fromList']/option[contains(.,'cdxml: ChemDraw CDXML')]").click()
+    driver.find_element(By.XPATH, "//select[@id='fromList']/option[contains(.,'cdxml: ChemDraw CDXML')]").click()
 
-        # Select InChI from the 'to' list.
+    # Select InChI from the 'to' list.
 
-        driver.find_element(By.XPATH, "//select[@id='toList']/option[contains(.,'inchi: InChI')]").click()
+    driver.find_element(By.XPATH, "//select[@id='toList']/option[contains(.,'inchi: InChI')]").click()
 
-        # Select Open Babel from the available conversion options list.
+    # Select Open Babel from the available conversion options list.
 
-        driver.find_element(By.XPATH, "//select[@id='success']/option[contains(.,'Open Babel')]").click()
+    driver.find_element(By.XPATH, "//select[@id='success']/option[contains(.,'Open Babel')]").click()
 
-        # Click on the "Yes" button.
+    # Click on the "Yes" button.
 
-        driver.find_element(By.XPATH, "//input[@id='yesButton']").click()
+    driver.find_element(By.XPATH, "//input[@id='yesButton']").click()
 
-        # Select the input file.
+    # Select the input file.
 
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='fileToUpload']")))
-        driver.find_element(By.XPATH, "//input[@id='fileToUpload']").send_keys(str(input_file))
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='fileToUpload']")))
+    driver.find_element(By.XPATH, "//input[@id='fileToUpload']").send_keys(str(input_file))
 
-        # Request the log file
+    # Request the log file
 
-        driver.find_element(By.XPATH, "//input[@id='requestLog']").click()
+    driver.find_element(By.XPATH, "//input[@id='requestLog']").click()
 
-        # Click on the "Convert" button.
+    # Click on the "Convert" button.
 
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='uploadButton']")))
-        driver.find_element(By.XPATH, "//input[@id='uploadButton']").click()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='uploadButton']")))
+    driver.find_element(By.XPATH, "//input[@id='uploadButton']").click()
 
-        # Handle alert box.
+    # Handle alert box.
 
-        WebDriverWait(driver, 10).until(EC.alert_is_present())
-        Alert(driver).dismiss()
+    WebDriverWait(driver, 10).until(EC.alert_is_present())
+    Alert(driver).dismiss()
 
-        # Wait until files exist.
+    # Wait until files exist.
 
-        time_elapsed = 0
-        while (not Path.is_file(log_file)) or (not Path.is_file(output_file)):
-            time.sleep(1)
-            time_elapsed += 1
-            if time_elapsed > TIMEOUT:
-                assert False, f"Download of {output_file} and {log_file} timed out"
-
+    time_elapsed = 0
+    while (not Path.is_file(log_file)) or (not Path.is_file(output_file)):
         time.sleep(1)
+        time_elapsed += 1
+        if time_elapsed > TIMEOUT:
+            assert False, f"Download of {output_file} and {log_file} timed out"
 
-        # Verify that the InChI file is correct.
+    time.sleep(1)
 
-        self.assertEqual(output_file.read_text().strip(), "InChI=1S/C12NO/c1-12(2)6-7-13-11-5-4-9(14-3)8-10(11)12")
+    # Verify that the InChI file is correct.
 
-
-if __name__ == '__main__':
-    unittest.main()
+    assert output_file.read_text().strip() == "InChI=1S/C12NO/c1-12(2)6-7-13-11-5-4-9(14-3)8-10(11)12"
