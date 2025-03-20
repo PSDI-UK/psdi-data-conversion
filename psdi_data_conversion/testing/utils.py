57240f7756d15b2c0844585ad1713b25e97fe2c8
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-03-20 15:31:04 +0000

    Set up to allow iterating over test specs

--- psdi_data_conversion/testing/utils.py
+++ psdi_data_conversion/testing/utils.py
@@ -80,6 +80,10 @@ class GUIConversionTestInfo(ConversionTestInfo):
 @dataclass
 class ConversionTestSpec:
     """Class providing a specification for a test file conversion.
+
+    All attributes of this class can be provided either as a single value or a list of values. In the case that a list
+    is provided for one or more attributes, the lists must all be the same length, and they will be iterated through
+    (as if using zip on the multiple lists) to test each element in turn.
     """
 
     filename: str | Iterable[str]
@@ -91,14 +95,126 @@ class ConversionTestSpec:
     name: str | Iterable[str] = CONVERTER_DEFAULT
     """The name of the converter to be used for the test, or a list thereof"""
 
-    conversion_kwargs: dict[str, Any] | list[dict[str, Any]] = field(default_factory=dict)
+    conversion_kwargs: dict[str, Any] | Iterable[dict[str, Any]] = field(default_factory=dict)
     """Any keyword arguments to be provided to the call to `run_converter`, aside from those listed above, or a list
     thereof"""
+
+    expect_success: bool | Iterable[bool] = True
+    """Whether or not to expect the test to succeed"""
+
+    post_conversion_callback: (Callable[[ConversionTestInfo], str] |
+                               Iterable[Callable[[ConversionTestInfo], str]] | None) = None
+    """Function to be called after the conversion is performed to check in detail whether results are as expected. It
+    should take as its only argument a `ConversionTestInfo` and return a string. The string should be empty if the check
+    is passed and should explain the failure otherwise."""
+
+    def __post_init__(self):
+        """Regularize the lengths of all attribute lists, in case some were provided as single values and others as
+        lists, and set up initial values
+        """
+
+        # To ease maintainability, we get the list of this class's attributes automatically from its __dict__, excluding
+        # any which start with an underscore
+        self._l_attr_names: list[str] = [attr_name for attr_name in self.__dict__ if not attr_name.startswith("_")]
+
+        l_single_val_attrs = []
+        self._len: int = 1
+
+        # Check if each attribute of this class is provided as a list, and if any are, make sure that all lists are
+        # the same length
+        for attr_name in self._l_attr_names:
+            val = getattr(self, attr_name)
+
+            val_len = 1
+
+            # Check first if the attr is a str or a dict, which are iterable, but are single-values for the purpose
+            # of values here
+            if isinstance(val, (str, dict)):
+                l_single_val_attrs.append(attr_name)
+            else:
+                # It's not a str or a dict, so test if we can get the length of it, which indicates it is iterable
+                try:
+                    val_len = len(val)
+                    # If it's a single value in a list, unpack it for now
+                    if val_len == 1:
+                        # Pylint for some reason things `Any` objects aren't subscriptable, but here we know it is
+                        val: Iterable[Any]
+                        setattr(self, attr_name, val[0])
+                except TypeError:
+                    l_single_val_attrs.append(attr_name)
+
+            # Check if there are any conflicts with some lists being provided as different lengths
+            if (self._len > 1) and (val_len > 1) and (val_len != self._len):
+                raise ValueError("All lists of values which are set as attributes for a `ConversionTestSpec` must be "
+                                 "the same length.")
+            self._len = val_len
+
+        # At this point, self._len will be either 1 if all attrs are single values, or the length of the lists for attrs
+        # that aren't. To keep everything regularised, we make everything a list of this length
+        for attr_name in self._l_attr_names:
+            if attr_name in l_single_val_attrs:
+                setattr(self, attr_name, [attr_name]*self._len)
+
+    def __len__(self):
+        """Get the length from the member - valid only after `__post_init__` has been called"""
+        return self._len
+
+    def __iter__(self):
+        """Allow to iterate over the class, getting a `SingleConversionTestSpec` for each value
+        """
+        l_l_attr_vals = zip(*[getattr(self, attr_name) for attr_name in self._l_attr_names])
+        while True:
+            l_attr_vals = next(l_l_attr_vals)
+            yield _SingleConversionTestSpec(**dict(zip(self._l_attr_names, l_attr_vals)))
+
+
+@dataclass
+class _SingleConversionTestSpec:
+    """Class providing a specification for a single test file conversion, produced by iterating over a
+    `ConversionTestSpec` object
+    """
+
+    filename: str
+    """The name of the input file, relative to the input test data location"""
+
+    to_format: str
+    """The format to test converting the input file to"""
+
+    name: str | Iterable[str] = CONVERTER_DEFAULT
+    """The name of the converter to be used for the test"""
+
+    conversion_kwargs: dict[str, Any] = field(default_factory=dict)
+    """Any keyword arguments to be provided to the call to `run_converter`, aside from those listed above"""
 
     expect_success: bool = True
     """Whether or not to expect the test to succeed"""
 
-    post_conversion_callback: Callable[[ConversionTestInfo], str] | None = None
+    post_conversion_callback: (Callable[[ConversionTestInfo], str] | None) = None
     """Function to be called after the conversion is performed to check in detail whether results are as expected. It
     should take as its only argument a `ConversionTestInfo` and return a string. The string should be empty if the check
     is passed and should explain the failure otherwise."""
+
+
+def run_test_conversion_with_library(test_spec: ConversionTestSpec):
+    """Runs a test conversion or series thereof through a call to the python library's `run_converter` function.
+
+    Parameters
+    ----------
+    test_spec : ConversionTestSpec
+        The specification for the test or series of tests to be run
+    """
+
+    # Iterate over the test spec to run each individual test it defines
+    for single_test_spec in test_spec:
+        _run_single_test_conversion_with_library(single_test_spec)
+
+
+def _run_single_test_conversion_with_library(single_test_spec: _SingleConversionTestSpec):
+    """Runs a single test conversion through a call to the python library's `run_converter` function.
+
+    Parameters
+    ----------
+    single_test_spec : _SingleConversionTestSpec
+        The specification for the test to be run
+    """
+    pass
