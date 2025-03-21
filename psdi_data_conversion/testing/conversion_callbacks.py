c06680d672819c6ab4c831475883e567d1381f0a
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-03-21 17:18:40 +0000

    Add callback for checking logs

--- psdi_data_conversion/testing/conversion_callbacks.py
+++ psdi_data_conversion/testing/conversion_callbacks.py
@@ -6,10 +6,14 @@ conversion test, run with the functions and classes defined in the `utils.py` mo
 """
 
 from collections.abc import Callable, Iterable
-from dataclasses import dataclass
+from copy import deepcopy
+from dataclasses import dataclass, field
 from genericpath import isfile
 import os
+import re
 
+from psdi_data_conversion.constants import DATETIME_RE_RAW
+from psdi_data_conversion.log_utility import string_with_placeholders_matches
 from psdi_data_conversion.testing.utils import ConversionTestInfo
 
 
@@ -88,6 +92,98 @@ class CheckOutputStatus:
         elif os.path.isfile(qualified_global_log_filename):
             l_errors.append(f"ERROR: Global log file from conversion '{qualified_global_log_filename}' exists, but was "
                             "expected to not exist")
+
+        # Join any errors for output
+        res = "\n".join(l_errors)
+        return res
+
+
+@dataclass
+class CheckLogContents:
+    """Callable class which checks the contents of the output log of a conversion"""
+
+    disable_default_checks: bool = False
+    """If set to true, default checks on the log contents will not be run (such as checking that the filename is
+    present and there are no errors)
+    """
+
+    l_strings_to_find: Iterable[str] = field(default_factory=list)
+    """List of any strings which must be found in the log. These may optionally include formatting placeholders,
+    e.g. "The filename is: {file}", in which case any text in the place of the placeholder will be considered valid for
+    a match.
+    """
+
+    l_strings_to_exclude: Iterable[str] = field(default_factory=list)
+    """List of any strings which must NOT be found in the log. These may optionally include formatting placeholders,
+    e.g. "The filename is: {file}", in which case any text in the place of the placeholder will be considered valid for
+    a match.
+    """
+
+    l_regex_to_find: Iterable[str] = field(default_factory=list)
+    """List of uncompiled regular expressions which must be matched somewhere in the log"""
+
+    l_regex_to_exclude: Iterable[str] = field(default_factory=list)
+    """List of uncompiled regular expressions which must NOT be matched anywhere in the log"""
+
+    def __call__(self, test_info: ConversionTestInfo) -> str:
+        """Perform the check on log contents"""
+
+        # First, check that the log exists
+        qualified_log_filename = test_info.qualified_log_filename
+        if not os.path.isfile(qualified_log_filename):
+            return f"ERROR: Expected log file from conversion '{qualified_log_filename}' does not exist"
+
+        log_text = open(qualified_log_filename, "r").read()
+
+        l_errors: list[str] = []
+
+        # Unless default checks are disabled, add them to the lists of strings/regexes to check
+        if self.disable_default_checks:
+            l_strings_to_find = self.l_strings_to_find
+            l_strings_to_exclude = self.l_strings_to_exclude
+            l_regex_to_find = self.l_regex_to_find
+            l_regex_to_exclude = self.l_regex_to_exclude
+
+        if not self.disable_default_checks:
+
+            l_strings_to_find = self.l_strings_to_find
+
+            # Make sure there's no error listed in the log
+            l_strings_to_exclude = deepcopy(list(self.l_strings_to_exclude))
+            l_strings_to_exclude.append("ERROR")
+
+            # Make sure the log includes the filename and timestamp
+            l_regex_to_find = deepcopy(list(self.l_regex_to_find))
+            l_regex_to_find.append(r"File name:\s+"+os.path.splitext(test_info.test_spec.filename)[0])
+            l_regex_to_find.append(DATETIME_RE_RAW)
+
+            l_regex_to_exclude = self.l_regex_to_exclude
+
+        # Check that all expected strings are present
+        for string_to_find in l_strings_to_find:
+            if not string_with_placeholders_matches(string_to_find, log_text):
+                l_errors.append(f"ERROR: String \"{string_to_find}\" was expected in log file "
+                                f"'{qualified_log_filename}' but was not found")
+
+        # Check that all excluded strings are not present
+        for l_strings_to_exclude in l_strings_to_exclude:
+            if string_with_placeholders_matches(l_strings_to_exclude, log_text):
+                l_errors.append(f"ERROR: String \"{l_strings_to_exclude}\" was not expected in log file "
+                                f"'{qualified_log_filename}' but was found")
+
+        # Check that all expected regexes are present
+        for regex_to_find in l_regex_to_find:
+            compiled_regex = re.compile(regex_to_find)
+            if not compiled_regex.search(log_text):
+                l_errors.append(f"ERROR: Regex /{regex_to_find}/ was expected in log file "
+                                f"'{qualified_log_filename}' but was not found")
+
+        # Check that all excluded regexes are not present
+        for regex_to_exclude in l_regex_to_exclude:
+            compiled_regex = re.compile(regex_to_find)
+            if compiled_regex.search(log_text):
+                l_errors.append(f"ERROR: Regex /{regex_to_exclude}/ was not expected in log file "
+                                f"'{qualified_log_filename}' but was found")
 
         # Join any errors for output
         res = "\n".join(l_errors)
