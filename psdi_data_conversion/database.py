aef9a743263f6c982328d7e8cc278d34a03a8f46
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-05-15 15:09:49 +0100

    Use more standard calling signature

--- psdi_data_conversion/database.py
+++ psdi_data_conversion/database.py
@@ -718,7 +718,7 @@ class ConversionsTable:
     def _get_possible_converters(self, in_format_info: FormatInfo, out_format_info: FormatInfo):
         """Get a list of all converters which can convert from one converter to another
         """
-        l_edges = self.graph.es.select(lambda x: x.source == in_format_info.id and x.target == out_format_info.id)
+        l_edges = self.graph.es.select(_source=in_format_info.id, _target=out_format_info.id)
         return [x[DB_NAME_KEY] for x in l_edges]
 
     def get_possible_conversions(self,
