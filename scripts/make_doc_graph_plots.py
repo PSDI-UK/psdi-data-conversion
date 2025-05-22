0131bc616a15972b73337cff82e6911fa49d769d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-05-22 10:41:40 +0100

    Set up for the values we want

--- scripts/make_doc_graph_plots.py
+++ scripts/make_doc_graph_plots.py
@@ -1,20 +1,17 @@
 import igraph as ig
 import matplotlib.pyplot as plt
 
-# Construct a graph with 5 vertices
-n_vertices = 5
-edges = [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (3, 4)]
-g = ig.Graph(n_vertices, edges)
+CONVERTER_OB = "Open Babel"
+CONVERTER_ATO = "Atomsk"
 
-# Set attributes for the graph, nodes, and edges
-g["title"] = "Small Social Network"
-g.vs["name"] = ["Daniel Morillas", "Kathy Archer", "Kyle Ding", "Joshua Walton", "Jana Hoyer"]
-g.vs["gender"] = ["M", "F", "F", "M", "F"]
-g.es["married"] = [False, False, False, False, False, False, False, True]
+# Construct a graph with 4 vertices
+l_names = ["MOLDY", "CIF", "PDB", "InChI"]
+edges = [(0, 1), (0, 2), (1, 2), (1, 2), (1, 3), (2, 3)]
+l_converters = [CONVERTER_ATO, CONVERTER_ATO, CONVERTER_ATO, CONVERTER_OB, CONVERTER_OB, CONVERTER_OB]
+g = ig.Graph(len(l_names), edges, vertex_attrs={"name": l_names}, edge_attrs={"converter": l_converters})
 
-# Set individual attributes
-g.vs[1]["name"] = "Kathy Morillas"
-g.es[0]["married"] = True
+# Set title for the graph
+g["title"] = "Example conversions"
 
 # Plot in matplotlib
 # Note that attributes can be set globally (e.g. vertex_size), or set individually using arrays (e.g. vertex_color)
@@ -22,18 +19,20 @@ fig, ax = plt.subplots(figsize=(5, 5))
 ig.plot(
     g,
     target=ax,
-    layout="circle",  # print nodes in a circular layout
+    # layout="circle",  # print nodes in a circular layout
     vertex_size=30,
-    vertex_color=["steelblue" if gender == "M" else "salmon" for gender in g.vs["gender"]],
+    vertex_color="steelblue",
     vertex_frame_width=4.0,
     vertex_frame_color="white",
     vertex_label=g.vs["name"],
     vertex_label_size=7.0,
-    edge_width=[2 if married else 1 for married in g.es["married"]],
-    edge_color=["#7142cf" if married else "#AAA" for married in g.es["married"]]
+    # edge_width=[2 if married else 1 for married in g.es["married"]],
+    edge_color=["#7142cf" if converter == CONVERTER_ATO else "#AAA" for converter in g.es["converter"]]
 )
 
 plt.show()
+
+exit()
 
 # Save the graph as an image file
 fig.savefig('social_network.png')
