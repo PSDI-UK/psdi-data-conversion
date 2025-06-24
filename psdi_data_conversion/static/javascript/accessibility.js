d2dee46a58c6356bf7bfb69a4c367a29b4bcf131
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2025-06-24 16:29:59 +0100

    Fix a couple more default boxes not displaying properly

--- psdi_data_conversion/static/javascript/accessibility.js
+++ psdi_data_conversion/static/javascript/accessibility.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
 
     // Store default styles in the stylesheet
     r.style.setProperty('--psdi-default-font', sessionStorage.getItem('psdi-default-font'));
+    r.style.setProperty('--psdi-default-font-size', sessionStorage.getItem('psdi-default-font-size'));
     r.style.setProperty('--psdi-default-heading-font', sessionStorage.getItem('psdi-default-heading-font'));
     r.style.setProperty('--psdi-default-letter-spacing', sessionStorage.getItem('psdi-default-letter-spacing'));
     r.style.setProperty('--psdi-default-font-weight', sessionStorage.getItem('psdi-default-font-weight'));
