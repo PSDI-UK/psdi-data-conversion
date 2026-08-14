ba0bf22c08c9475279e34d97b3d891d73415eb3a
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2026-08-14 16:33:20 +0100

    Clean up imports

--- tests/python/database_test.py
+++ tests/python/database_test.py
@@ -12,10 +12,6 @@ import pytest
 from psdi_data_conversion import constants as const
 from psdi_data_conversion import database as db
 from psdi_data_conversion.converter import L_SUPPORTED_CONVERTERS
-from psdi_data_conversion.database import (FileConverterDatabaseException, FormatInfo, disambiguate_formats,
-                                           get_conversion_pathway, get_conversion_quality, get_converter_info,
-                                           get_database, get_format_info, get_in_format_args, get_out_format_args,
-                                           get_possible_conversions, get_possible_formats)
 from psdi_data_conversion.testing import constants as tc
 from psdi_data_conversion.utils import regularize_name
 
@@ -24,8 +20,8 @@ def test_load():
     """Test that we can load and retrieve the database
     """
 
-    db1 = get_database()
-    db2 = get_database()
+    db1 = db.get_database()
+    db2 = db.get_database()
 
     # We should only get one database created, and any additional calls to `get_database()` should return the same
     assert db2 is db1
@@ -33,14 +29,14 @@ def test_load():
 
 @pytest.fixture(scope="module")
 def database():
-    return get_database()
+    return db.get_database()
 
 
 def test_converter_info(database):
     """Test that we can get the expected information on each converter
     """
 
-    l_converter_info = get_converter_info()
+    l_converter_info = db.get_converter_info()
     l_converter_names = [x.name for x in l_converter_info]
 
     # Check that all supported converters are in this list
@@ -78,13 +74,13 @@ def test_converter_info(database):
         assert "http" in converter_info.url
 
         # Check that this converter's info can be retrieved through all supported methods
-        assert converter_info is get_converter_info(converter_info)
-        assert converter_info is get_converter_info(converter_info.id)
-        assert converter_info is get_converter_info(UUID(int=converter_info.id))
-        assert converter_info is get_converter_info(str(UUID(int=converter_info.id)))
-        assert converter_info is get_converter_info(UUID(int=converter_info.id).hex)
-        assert converter_info is get_converter_info(converter_info.name)
-        assert converter_info is get_converter_info(converter_info.pretty_name)
+        assert converter_info is db.get_converter_info(converter_info)
+        assert converter_info is db.get_converter_info(converter_info.id)
+        assert converter_info is db.get_converter_info(UUID(int=converter_info.id))
+        assert converter_info is db.get_converter_info(str(UUID(int=converter_info.id)))
+        assert converter_info is db.get_converter_info(UUID(int=converter_info.id).hex)
+        assert converter_info is db.get_converter_info(converter_info.name)
+        assert converter_info is db.get_converter_info(converter_info.pretty_name)
 
 
 def test_format_args():
@@ -94,8 +90,8 @@ def test_format_args():
     in_format = tc.FORMAT_PDB_0
     out_format = tc.FORMAT_CIF
 
-    l_in_flags, _ = get_in_format_args(converter_name, in_format)
-    l_out_flags, _ = get_out_format_args(converter_name, out_format)
+    l_in_flags, _ = db.get_in_format_args(converter_name, in_format)
+    l_out_flags, _ = db.get_out_format_args(converter_name, out_format)
 
     l_in_flag_names = [x.flag for x in l_in_flags]
     l_out_flag_names = [x.flag for x in l_out_flags]
@@ -108,9 +104,9 @@ def test_format_args():
 
     # Check that we can find a specific argument
     in_flag_info_0 = l_in_flags[0]
-    assert get_in_format_args(converter_name, in_format, in_flag_info_0.flag) is in_flag_info_0
+    assert db.get_in_format_args(converter_name, in_format, in_flag_info_0.flag) is in_flag_info_0
     out_flag_info_0 = l_out_flags[0]
-    assert get_out_format_args(converter_name, out_format, out_flag_info_0.flag) is out_flag_info_0
+    assert db.get_out_format_args(converter_name, out_format, out_flag_info_0.flag) is out_flag_info_0
 
     # Check that the UUID is constructed appropriately for the info objects
     assert in_flag_info_0.uuid == UUID(int=in_flag_info_0.id)
@@ -124,7 +120,7 @@ def test_format_info(database):
     for name, id in (("pdb", tc.FORMAT_PDB_0), ("cif", tc.FORMAT_CIF), ("mmcif", tc.FORMAT_MMCIF),
                      ("inchi", tc.FORMAT_INCHI), ("molreport", tc.FORMAT_MOLREPORT)):
 
-        format_info = get_format_info(id)
+        format_info = db.get_format_info(id)
 
         # Check database is properly set as parent
         assert format_info.parent == database
@@ -136,13 +132,13 @@ def test_format_info(database):
         assert format_info.uuid == UUID(int=format_info.id)
 
         # Check that this format's info can be retrieved through all supported methods
-        assert format_info is get_format_info(format_info)
-        assert format_info is get_format_info(format_info.id)
-        assert format_info is get_format_info(UUID(int=format_info.id))
-        assert format_info is get_format_info(str(UUID(int=format_info.id)))
-        assert format_info is get_format_info(UUID(int=format_info.id).hex)
-        assert format_info is get_format_info(format_info.name, which=0)
-        assert format_info is get_format_info(format_info.disambiguated_name)
+        assert format_info is db.get_format_info(format_info)
+        assert format_info is db.get_format_info(format_info.id)
+        assert format_info is db.get_format_info(UUID(int=format_info.id))
+        assert format_info is db.get_format_info(str(UUID(int=format_info.id)))
+        assert format_info is db.get_format_info(UUID(int=format_info.id).hex)
+        assert format_info is db.get_format_info(format_info.name, which=0)
+        assert format_info is db.get_format_info(format_info.disambiguated_name)
 
         # Check properties are as expected
 
@@ -172,29 +168,29 @@ def test_format_info_options():
     """
 
     # Check that we get an exception for an ambiguous format if we don't request which
-    with pytest.raises(FileConverterDatabaseException):
-        get_format_info("pdb")
+    with pytest.raises(db.FileConverterDatabaseException):
+        db.get_format_info("pdb")
 
     # Check that requesting all possibilities works as expected
-    l_pdb_infos = get_format_info("pdb", which="all")
+    l_pdb_infos = db.get_format_info("pdb", which="all")
     assert l_pdb_infos[0] != l_pdb_infos[1]
-    assert l_pdb_infos[0] == get_format_info("pdb", which=0)
-    assert l_pdb_infos[1] == get_format_info("pdb", which=1)
+    assert l_pdb_infos[0] == db.get_format_info("pdb", which=0)
+    assert l_pdb_infos[1] == db.get_format_info("pdb", which=1)
 
     # Check that the shortcut for which format works
-    assert get_format_info("pdb-0") == l_pdb_infos[0]
-    assert get_format_info("pdb-1") == l_pdb_infos[1]
+    assert db.get_format_info("pdb-0") == l_pdb_infos[0]
+    assert db.get_format_info("pdb-1") == l_pdb_infos[1]
 
     # Check that the shortcut doesn't cause any problems even if the format is unambiguous
-    assert get_format_info("cif-0") == get_format_info("cif")
+    assert db.get_format_info("cif-0") == db.get_format_info("cif")
 
     # Check that the format info provides the right disambiguated name
-    assert get_format_info("cif").disambiguated_name == "cif"
-    assert get_format_info("pdb-0").disambiguated_name == "pdb-0"
-    assert get_format_info("pdb-1").disambiguated_name == "pdb-1"
+    assert db.get_format_info("cif").disambiguated_name == "cif"
+    assert db.get_format_info("pdb-0").disambiguated_name == "pdb-0"
+    assert db.get_format_info("pdb-1").disambiguated_name == "pdb-1"
 
     # Check that formats are case-insensitive
-    assert get_format_info("PDB-0") is get_format_info("pdb-0")
+    assert db.get_format_info("PDB-0") is db.get_format_info("pdb-0")
 
 
 def test_disambiguate_format():
@@ -202,17 +198,17 @@ def test_disambiguate_format():
     """
 
     # Test that we can disambiguate the right pdb format
-    in_format, out_format = disambiguate_formats(const.CONVERTER_OB, "pdb", "cif")
-    assert in_format == get_format_info(tc.FORMAT_PDB_0)
-    assert out_format == get_format_info(tc.FORMAT_CIF)
+    in_format, out_format = db.disambiguate_formats(const.CONVERTER_OB, "pdb", "cif")
+    assert in_format == db.get_format_info(tc.FORMAT_PDB_0)
+    assert out_format == db.get_format_info(tc.FORMAT_CIF)
 
     # Test we get the expected exception if no conversion is possible
-    with pytest.raises(FileConverterDatabaseException, match="is not supported"):
-        disambiguate_formats(const.CONVERTER_C2X, "ins", "cml")
+    with pytest.raises(db.FileConverterDatabaseException, match="is not supported"):
+        db.disambiguate_formats(const.CONVERTER_C2X, "ins", "cml")
 
     # Test we get the expected exception if multiple conversions are possible
-    with pytest.raises(FileConverterDatabaseException, match="is ambiguous"):
-        disambiguate_formats(const.CONVERTER_C2X, "cif", "pdb")
+    with pytest.raises(db.FileConverterDatabaseException, match="is ambiguous"):
+        db.disambiguate_formats(const.CONVERTER_C2X, "cif", "pdb")
 
 
 def test_conversion_table(database):
@@ -224,19 +220,19 @@ def test_conversion_table(database):
     assert conversions_table.parent is database
 
     # Check we can get the correct conversion quality
-    assert get_conversion_quality(const.CONVERTER_OB, tc.FORMAT_PDB_0, tc.FORMAT_CIF).qual_str == const.QUAL_UNKNOWN
-    assert get_conversion_quality(const.CONVERTER_ATO, tc.FORMAT_XYZ_1, tc.FORMAT_INCHI) is None
+    assert db.get_conversion_quality(const.CONVERTER_OB, tc.FORMAT_PDB_0, tc.FORMAT_CIF).qual_str == const.QUAL_UNKNOWN
+    assert db.get_conversion_quality(const.CONVERTER_ATO, tc.FORMAT_XYZ_1, tc.FORMAT_INCHI) is None
 
     # Do some detailed checks on one conversion
-    xyz_format_info = get_format_info(tc.FORMAT_XYZ_1)
-    inchi_format_info = get_format_info(tc.FORMAT_INCHI)
+    xyz_format_info = db.get_format_info(tc.FORMAT_XYZ_1)
+    inchi_format_info = db.get_format_info(tc.FORMAT_INCHI)
 
     # "xyz" is ambiguous, but only one possibility has a valid conversion here, so check that we get that one
-    qual = get_conversion_quality(const.CONVERTER_OB, "xyz", inchi_format_info)
+    qual = db.get_conversion_quality(const.CONVERTER_OB, "xyz", inchi_format_info)
 
     assert qual.qual_str == const.QUAL_OKAY
-    assert qual.in_format is get_format_info(xyz_format_info)
-    assert qual.out_format is get_format_info(inchi_format_info)
+    assert qual.in_format is db.get_format_info(xyz_format_info)
+    assert qual.out_format is db.get_format_info(inchi_format_info)
 
     details = qual.details
 
@@ -259,14 +255,14 @@ def test_conversion_table(database):
     assert comp_prop_info.note == ""
 
     # Check we can get a list of possible converters for a given conversion
-    l_possible_conversions = get_possible_conversions("pdb", "cif")
-    assert (get_converter_info(const.CONVERTER_OB), get_format_info("pdb", which=0),
-            get_format_info("cif", which=0)) in l_possible_conversions
+    l_possible_conversions = db.get_possible_conversions("pdb", "cif")
+    assert (db.get_converter_info(const.CONVERTER_OB), db.get_format_info("pdb", which=0),
+            db.get_format_info("cif", which=0)) in l_possible_conversions
 
     # Check that we can get a list of possible input/outpat formats for a given converter
-    l_in_formats, l_out_formats = get_possible_formats(const.CONVERTER_OB)
-    assert get_format_info(tc.FORMAT_PDB_0) in l_in_formats
-    assert get_format_info(tc.FORMAT_CIF) in l_out_formats
+    l_in_formats, l_out_formats = db.get_possible_formats(const.CONVERTER_OB)
+    assert db.get_format_info(tc.FORMAT_PDB_0) in l_in_formats
+    assert db.get_format_info(tc.FORMAT_CIF) in l_out_formats
 
 
 def test_conversion_pathways():
@@ -274,13 +270,13 @@ def test_conversion_pathways():
     """
 
     # Check that we get `None` for converting from one format to itself
-    assert get_conversion_pathway(tc.FORMAT_CIF, tc.FORMAT_CIF) is None
+    assert db.get_conversion_pathway(tc.FORMAT_CIF, tc.FORMAT_CIF) is None
 
     # Check that we get `None` for an impossible conversion
-    assert get_conversion_pathway(tc.FORMAT_CIF, tc.FORMAT_ABINIT) is None
+    assert db.get_conversion_pathway(tc.FORMAT_CIF, tc.FORMAT_ABINIT) is None
 
     # Check that we get the expected single-step conversion for a known direct conversion
-    cif_to_inchi_path = get_conversion_pathway(tc.FORMAT_CIF, tc.FORMAT_INCHI, only="registered")
+    cif_to_inchi_path = db.get_conversion_pathway(tc.FORMAT_CIF, tc.FORMAT_INCHI, only="registered")
     assert len(cif_to_inchi_path) == 1
     converter_info, in_format_info, out_format_info = cif_to_inchi_path[0]
     assert converter_info.name == regularize_name(const.CONVERTER_OB)
@@ -289,7 +285,7 @@ def test_conversion_pathways():
 
     # Test getting a multi-step conversion - it's possible this will become direct in the future if a new converter is
     # added, so the test is a bit loose here
-    inchi_to_moldy_path = get_conversion_pathway(tc.FORMAT_INCHI, tc.FORMAT_MOLDY)
+    inchi_to_moldy_path = db.get_conversion_pathway(tc.FORMAT_INCHI, tc.FORMAT_MOLDY)
     assert len(inchi_to_moldy_path) <= 2
     assert inchi_to_moldy_path[0][1].id == tc.FORMAT_INCHI
     assert inchi_to_moldy_path[-1][2].id == tc.FORMAT_MOLDY
@@ -302,17 +298,17 @@ def test_conversion_pathways():
 
 @pytest.fixture()
 def format_all(scope="module"):
-    return FormatInfo("all", database, {key: True for key in db.D_PROP_BITS.keys()})
+    return db.FormatInfo("all", database, {key: True for key in db.D_PROP_BITS.keys()})
 
 
 @pytest.fixture(scope="module")
 def format_none(database):
-    return FormatInfo("none", database, {key: False for key in db.D_PROP_BITS.keys()})
+    return db.FormatInfo("none", database, {key: False for key in db.D_PROP_BITS.keys()})
 
 
 @pytest.fixture(scope="module")
 def format_unknown(database):
-    return FormatInfo("unknown", database, {key: None for key in db.D_PROP_BITS.keys()})
+    return db.FormatInfo("unknown", database, {key: None for key in db.D_PROP_BITS.keys()})
 
 
 @pytest.fixture(scope="module")
@@ -348,8 +344,8 @@ def test_get_conversion_prop_weight(in_format, out_format, ex_weight, request):
 @pytest.mark.parametrize("prop", db.D_PROP_BITS.keys())
 def test_get_conversion_prop_weight_prop_lost(format_none, prop):
     """Test each property individually when it's lost to ensure the right bit is set for each"""
-    test_format = FormatInfo("test", database, {key: True if key == prop else False
-                                                for key in db.D_PROP_BITS.keys()})
+    test_format = db.FormatInfo("test", database, {key: True if key == prop else False
+                                                   for key in db.D_PROP_BITS.keys()})
     assert db.get_conversion_prop_weight(test_format, format_none) == db.STEP_WEIGHT | 1 << db.D_PROP_BITS[prop]
 
 
@@ -361,6 +357,6 @@ def test_get_conversion_prop_weight_prop_lost(format_none, prop):
                                                           (24, 30, 1 << 0*db.PREC_GAP_BITS),
                                                           (24, 6, 1 << db.PREC_MAX_DIGIT_LOSS*db.PREC_GAP_BITS)])
 def test_get_conversion_precision_weight(database, in_prec, out_prec, ex_weight):
-    in_format = FormatInfo("in", database, {db.DB_FORMAT_PRECISION_KEY: in_prec})
-    out_format = FormatInfo("in", database, {db.DB_FORMAT_PRECISION_KEY: out_prec})
+    in_format = db.FormatInfo("in", database, {db.DB_FORMAT_PRECISION_KEY: in_prec})
+    out_format = db.FormatInfo("in", database, {db.DB_FORMAT_PRECISION_KEY: out_prec})
     assert db.get_conversion_precision_weight(in_format, out_format) == ex_weight
