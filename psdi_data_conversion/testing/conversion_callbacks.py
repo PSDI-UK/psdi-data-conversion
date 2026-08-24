752629f9f28dc16abd88d8ba21c1a7904c8a91b5
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2026-08-24 14:59:05 +0100

    Allow test to check input and output files

--- psdi_data_conversion/testing/conversion_callbacks.py
+++ psdi_data_conversion/testing/conversion_callbacks.py
@@ -60,17 +60,27 @@ class CheckFileStatus:
     expect_global_log_exists: bool | None = None
     """Whether to expect that the global log exists (zero-size allowed) or not. If None, will not check either way."""
 
-    expect_other_files_exist: list[str] | None = None
-    """A list of other files which are expected to exist"""
+    expect_input_files_exist: list[str] | None = None
+    """A list of input files which are expected to exist"""
 
-    expect_other_files_not_exist: list[str] | None = None
-    """A list of other files which are expected to not exist"""
+    expect_input_files_not_exist: list[str] | None = None
+    """A list of input files which are expected to not exist"""
+
+    expect_output_files_exist: list[str] | None = None
+    """A list of output files which are expected to exist"""
+
+    expect_output_files_not_exist: list[str] | None = None
+    """A list of output files which are expected to not exist"""
 
     def __post_init__(self):
-        if self.expect_other_files_exist is None:
-            self.expect_other_files_exist = []
-        if self.expect_other_files_not_exist is None:
-            self.expect_other_files_not_exist = []
+        if self.expect_input_files_exist is None:
+            self.expect_input_files_exist = []
+        if self.expect_input_files_not_exist is None:
+            self.expect_input_files_not_exist = []
+        if self.expect_output_files_exist is None:
+            self.expect_output_files_exist = []
+        if self.expect_output_files_not_exist is None:
+            self.expect_output_files_not_exist = []
 
     def __call__(self, test_info: ConversionTestInfo) -> str:
         """Perform the check on output file and log status"""
@@ -126,21 +136,38 @@ class CheckFileStatus:
             l_errors.append(f"ERROR: Global log file from conversion '{qualified_global_log_filename}' exists, but was "
                             "expected to not exist")
 
-        # Check the status of other files expected to exist
-        for other_file in self.expect_other_files_exist:
-            qualified_other_file = os.path.join(test_info.output_dir, other_file)
-            if not os.path.isfile(qualified_other_file):
-                l_errors.append(f"ERROR: Expected file for conversion '{qualified_other_file}' does not "
+        # Check the status of input files expected to exist
+        for input_file in self.expect_input_files_exist:
+            qualified_input_file = os.path.join(test_info.input_dir, input_file)
+            if not os.path.isfile(qualified_input_file):
+                l_errors.append(f"ERROR: Expected file for conversion '{qualified_input_file}' does not "
                                 "exist")
-            elif os.path.getsize(qualified_other_file) == 0:
-                l_errors.append(f"ERROR: Expected file for conversion '{qualified_other_file}' exists but "
+            elif os.path.getsize(qualified_input_file) == 0:
+                l_errors.append(f"ERROR: Expected file for conversion '{qualified_input_file}' exists but "
                                 "is unexpectedly empty")
 
-        # Check the status of other files expected to not exist
-        for other_file in self.expect_other_files_not_exist:
-            qualified_other_file = os.path.join(test_info.output_dir, other_file)
-            if os.path.isfile(qualified_other_file):
-                l_errors.append(f"ERROR: File from conversion '{qualified_other_file}' exists, but was "
+        # Check the status of input files expected to not exist
+        for input_file in self.expect_input_files_not_exist:
+            qualified_input_file = os.path.join(test_info.input_dir, input_file)
+            if os.path.isfile(qualified_input_file):
+                l_errors.append(f"ERROR: File from conversion '{qualified_input_file}' exists, but was "
+                                "expected to not exist")
+
+        # Check the status of output files expected to exist
+        for output_file in self.expect_output_files_exist:
+            qualified_output_file = os.path.join(test_info.output_dir, output_file)
+            if not os.path.isfile(qualified_output_file):
+                l_errors.append(f"ERROR: Expected file for conversion '{qualified_output_file}' does not "
+                                "exist")
+            elif os.path.getsize(qualified_output_file) == 0:
+                l_errors.append(f"ERROR: Expected file for conversion '{qualified_output_file}' exists but "
+                                "is unexpectedly empty")
+
+        # Check the status of output files expected to not exist
+        for output_file in self.expect_output_files_not_exist:
+            qualified_output_file = os.path.join(test_info.output_dir, output_file)
+            if os.path.isfile(qualified_output_file):
+                l_errors.append(f"ERROR: File from conversion '{qualified_output_file}' exists, but was "
                                 "expected to not exist")
 
         # Join any errors for output
