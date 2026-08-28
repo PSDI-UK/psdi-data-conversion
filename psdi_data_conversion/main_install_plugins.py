7729146ae44193403655edcdb710f0adbb1edf72
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2026-08-28 16:39:45 +0100

    Disallow conversions between formats which are aliases of each other

--- psdi_data_conversion/main_install_plugins.py
+++ psdi_data_conversion/main_install_plugins.py
@@ -526,6 +526,11 @@ def run_from_args(args):
             for in_id, out_id in product(s_in_ids, s_out_ids):
                 s_unsupported_conversions.add((in_id, out_id))
 
+        # Disallow any conversions between formats which are aliases of each other
+        for s_alias_formats in d_format_aliases.values():
+            for in_id, out_id in product(s_alias_formats, s_alias_formats):
+                s_unsupported_conversions.add(in_id, out_id)
+
         for in_id, out_id in product(s_in_formats, s_out_formats):
             # Skip conversions of any format to itself, any that are labeled as unsupported or supported (the latter
             # will be added in a separate loop to avoid duplicates)
